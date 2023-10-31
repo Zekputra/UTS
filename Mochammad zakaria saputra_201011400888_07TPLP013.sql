ce72d7244ce643ae9051f411168bd48ae071cec4
Zekputra <137857236+Zekputra@users.noreply.github.com>
2023-10-31 16:50:09 +0700

    Add files via upload

---
+++ Mochammad zakaria saputra_201011400888_07TPLP013.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.handphone (product,chipset,ram,rom,layar,harga) VALUES
+	 ('Redmi 12C','MediaTek Helio G85','3 GB','32 GB','6,71 inch','Rp. 1.149.000'),
+	 ('Redmi 10 5G','MediaTek Dimensity 700','6 GB','128 GB','6,58 inch','Rp. 1.899.000'),
+	 ('Redmi 10A','MediaTek Helio G25','3 GB','32 GB','6,53 inch','Rp. 1.099.000'),
+	 ('Redmi 12','MediaTek Helio G88','8 GB','128 GB','6,71 inch','Rp. 1.999.000'),
+	 ('Redmi Note 11 Pro 5G','Qualcomm Snapdragon 695','8 GB','128 GB','6,67 inch','Rp. 3.998.000'),
+	 ('Redmi Note 11 Pro','MediaTek Helio G96','6 GB','128 GB','6,67 inch','Rp. 3.120.000'),
+	 ('Redmi Note 10','Qualcomm Snapdragon 678','4 GB','64 GB','6,43 inch','Rp. 2.399.000'),
+	 ('Redmi 10 2022','MediaTek Helio G88','4 GB','64 GB','6,53 inch','Rp. 1.749.000'),
+	 ('Redmi 10C','Qualcomm Snapdragon 680','4 GB','128 GB','6,71 inch','Rp. 1.499.000'),
+	 ('Redmi Note 10 Pro','Qualcomm Snapdragon 732G','6 GB','128 GB','6,67 inch','Rp. 4.058.000');
